b8f4166b23d7de5d72768da87ffa7df6e63650fd
Ana- Becerril <ana.becbel@gmail.com>
2021-03-22 20:23:25 -0600

    added tittles and two inputs for search te painting art

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -8,11 +8,29 @@ function home (){
 
     const boxSearch=document.createElement("div");
     boxSearch.classList.add("box-search")
+
+    const tittleContainer=document.createElement("div");
+    tittleContainer.classList.add("tittle-container");
+    tittleContainer.innerHTML="Find Art"
+
+    const searchTittle=document.createElement("div")
+    searchTittle.classList.add("search-tittle");
+    searchTittle.innerHTML="Search by:"
+
+    var inputContainer=document.createElement("div");
+    inputContainer.classList.add("input-container");
+
+    var inputAuthor = document.createElement("input");
+    inputAuthor.type = "text";
+    var inputPaint = document.createElement("input");
+    inputPaint.type = "text";
+
     const boxView=document.createElement("div");
     boxView.classList.add("box-view");
 
-    
-   
+
+    inputContainer.append(inputAuthor,inputPaint);
+    boxSearch.append(tittleContainer, searchTittle,inputContainer);
     homeBox.append(boxSearch,boxView);
     homeContainer.append(homeBox);
    
