5139fc30dd50e17b9123274d6c7c387a879607d8
Ana- Becerril <ana.becbel@gmail.com>
2021-04-08 23:10:48 -0500

    i added a variable who take a random item from the  api

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -8,11 +8,11 @@ const displayImageHome = document.querySelector('.home-container')
 fetch(`https://www.rijksmuseum.nl/api/nl/collection?key=2VKRLCml&involvedMaker=${userValue}`)
   .then(response => response.json())
   .then(function(response) {
-    var arr=
+    var arr= response.artObjects;
     var randomItem = arr[Math.floor(Math.random()*arr.length)];
-    displayInfo.innerHTML=response.artObjects[1].longTitle
-    displayImageView.style.backgroundImage=`url(${response.artObjects[1].webImage.url})`
-    displayImageHome.style.backgroundImage=`url(${response.artObjects[1].webImage.url})`
+    displayInfo.innerHTML=response.artObjects[randomItem].longTitle
+    displayImageView.style.backgroundImage=`url(${response.artObjects[randomItem].webImage.url})`
+    displayImageHome.style.backgroundImage=`url(${response.artObjects[randomItem].webImage.url})`
     console.log(response)
 }).catch(Error => console.log(Error))
 console.log();
